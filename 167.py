566f3386a6e6e0ce1aee491f8b374a73801805be
Dzhorobekov Eldar Salmoorbekovich <edzhorobekov@ozon.ru>
2023-12-09 01:18:43 +0600

    add 167.py

---
+++ 167.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            cur_sum = numbers[left] + numbers[right]
+            if cur_sum < target:
+                if numbers[left] < numbers[right]:
+                    left += 1
+                else:
+                    right += 1
+            elif cur_sum > target:
+                if numbers[left] < numbers[right]:
+                    right -= 1
+                else:
+                    left += 1
+            else:
+                break
+
+
+        return [left+1, right+1]
