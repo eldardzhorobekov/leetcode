1d6825629ac420642f38abf8850f5c2ce18289d1
Dzhorobekov Eldar Salmoorbekovich <edzhorobekov@ozon.ru>
2023-12-25 19:06:01 +0600

    add 94.py

---
+++ 94.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        def traversal(node: Optional[TreeNode]) -> None:
+            if not node:
+                return
+            traversal(node.left)
+            ans.append(node.val)
+            traversal(node.right)
+        traversal(root)
+        return ans
